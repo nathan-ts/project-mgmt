fac577b5c9207e24e68b0a5d8ef1c26742e6b4e0
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-06-22 17:10:23 -0400

    add class

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -5,18 +5,36 @@ export interface Props {
   enthusiasmLevel?: number;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: Props) {
-  if (enthusiasmLevel <= 0) {
-    throw new Error("You could be a little more enthusiastic. :D");
-  }
+// function Hello({ name, enthusiasmLevel = 1 }: Props) {
+//   if (enthusiasmLevel <= 0) {
+//     throw new Error("You could be a little more enthusiastic. :D");
+//   }
 
-  return (
-    <div className="hello">
-      <div className="greeting">
-        Hello {name + getExclamationMarks(enthusiasmLevel)}
+//   return (
+//     <div className="hello">
+//       <div className="greeting">
+//         Hello {name + getExclamationMarks(enthusiasmLevel)}
+//       </div>
+//     </div>
+//   );
+// }
+
+class Hello extends React.Component<Props> {
+  render() {
+    const { name, enthusiasmLevel = 1 } = this.props;
+
+    if (enthusiasmLevel <= 0) {
+      throw new Error("You could be a little more enthusiastic. :D");
+    }
+
+    return (
+      <div className="hello">
+        <div className="greeting">
+          Hello {name + getExclamationMarks(enthusiasmLevel)}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Hello;
