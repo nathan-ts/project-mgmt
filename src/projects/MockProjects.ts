208747f7d5ce38d9d7e5af3feb1dda6aa1ac86c0
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-07-21 17:57:38 -0400

    display projects using embedded components

--- src/projects/MockProjects.ts
+++ src/projects/MockProjects.ts
@@ -6,7 +6,7 @@ export const MOCK_PROJECTS = [
     name: 'Bytecard',
     description:
       'Bytecard provides software developers, medium and small businesses, and digital nomads with a web based tool that enables significant cost savings.',
-    imageUrl: '/assets/placeimg_500_300_arch4.jpg',
+    imageUrl: '/placeimg_500_300_arch4.jpg',
     contractTypeId: 1,
     contractSignedOn: '2021-09-28T08:55:03.473Z',
     budget: 31052,
@@ -17,7 +17,7 @@ export const MOCK_PROJECTS = [
     name: 'Overhold',
     description:
       'Overhold is a great resource for finding out about the value of different investments and how to invest for maximum value.',
-    imageUrl: '/assets/placeimg_500_300_arch1.jpg',
+    imageUrl: '/placeimg_500_300_arch1.jpg',
     contractTypeId: 5,
     contractSignedOn: '2021-02-01T14:41:06.419Z',
     budget: 28661,
@@ -28,7 +28,7 @@ export const MOCK_PROJECTS = [
     name: 'Veribet',
     description:
       'Veribet is your direct access to sports betting, racing and card games.',
-    imageUrl: '/assets/placeimg_500_300_arch12.jpg',
+    imageUrl: '/placeimg_500_300_arch12.jpg',
     contractTypeId: 2,
     contractSignedOn: '2021-10-28T19:03:47.706Z',
     budget: 97541,
@@ -39,7 +39,7 @@ export const MOCK_PROJECTS = [
     name: 'Vagram',
     description:
       'Vagram is a platform for travel-lovers to plan trips and share experiences.',
-    imageUrl: '/assets/placeimg_500_300_arch5.jpg',
+    imageUrl: '/placeimg_500_300_arch5.jpg',
     contractTypeId: 6,
     contractSignedOn: '2021-12-30T15:32:08.344Z',
     budget: 31052,
@@ -50,7 +50,7 @@ export const MOCK_PROJECTS = [
     name: 'Sonair',
     description:
       'Sonair is a medical-grade, portable, non-invasive vaporizer that helps you better manage your asthma and COPD symptoms.',
-    imageUrl: '/assets/placeimg_500_300_arch12.jpg',
+    imageUrl: '/placeimg_500_300_arch12.jpg',
     contractTypeId: 4,
     contractSignedOn: '2021-01-09T06:15:46.944Z',
     budget: 41068,
@@ -61,7 +61,7 @@ export const MOCK_PROJECTS = [
     name: 'Tripledex',
     description:
       'Tripledex is a fan-made website that allows players to trade and compete with each other in the world of pokemon.',
-    imageUrl: '/assets/placeimg_500_300_arch1.jpg',
+    imageUrl: '/placeimg_500_300_arch1.jpg',
     contractTypeId: 2,
     contractSignedOn: '2021-09-01T15:44:36.035Z',
     budget: 91714,
--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -7,14 +7,22 @@ interface ProjectListProps {
 
 function ProjectList({ projects }: ProjectListProps) {
   return (
-    <>
-      <h1>Projects</h1>
-      <ul className="row">
-        {projects.map(project => 
-          <li key={project.id}>{project.name}</li>
-        )}
-      </ul>
-    </>
+    <div className="row">
+      {projects.map(project => 
+        <div className="cols-sm" key={project.id}>
+          <div className="card">
+            <img src={project.imageUrl} alt={project.name} />
+            <section className="section dark">
+              <h5 className="strong">
+                <strong>{project.name}</strong>
+              </h5>
+              <p>{project.description}</p>
+              <p>Budget : ${project.budget.toLocaleString()}</p>
+            </section>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
