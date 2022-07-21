56a25640eb830d36d2426a20212932de60227088
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-07-21 15:23:45 -0400

    add bind event

--- src/index.tsx
+++ src/index.tsx
@@ -15,34 +15,50 @@ root.render(
   </React.StrictMode>
 );
 
-function FruitListItem(props: { fruit: { id: number; name: any; }; }) {
-  function handleClick(e: React.MouseEvent<HTMLLIElement, MouseEvent>, id: number) {
-    console.log(e);
-    console.log(`removed ${id}`);
+// function FruitListItem(props: { fruit: { id: number; name: any; }; }) {
+//   function handleClick(e: React.MouseEvent<HTMLLIElement, MouseEvent>, id: number) {
+//     console.log(e);
+//     console.log(`removed ${id}`);
+//   }
+
+//   return (
+//     <li onClick={(e) => handleClick(e, props.fruit.id)}>
+//       {props.fruit.name}{' '}
+//     </li>
+//   );
+// }
+
+// function FruitList(props: { fruits: any[]; }) {
+//   const fruitListItems = props.fruits.map((fruit: { id: any; name: any }) => (
+//     <FruitListItem key={fruit.id} fruit={fruit} />
+//   ));
+//   return <ul>{fruitListItems}</ul>;
+// }
+
+// const data = [
+//   { id: 1, name: 'apple' },
+//   { id: 2, name: 'orange' },
+//   { id: 3, name: 'blueberry' },
+//   { id: 4, name: 'banana' },
+//   { id: 5, name: 'kiwi' },
+// ];
+
+// root.render(
+//   <FruitList fruits={data} />
+// );
+
+class Button extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  return (
-    <li onClick={(e) => handleClick(e, props.fruit.id)}>
-      {props.fruit.name}{' '}
-    </li>
-  );
+  handleClick() {
+    console.log('clicked');
+  }
+
+  render() {
+    return <button onClick={this.handleClick}>Click Me!</button>;
+  }
 }
-
-function FruitList(props: { fruits: any[]; }) {
-  const fruitListItems = props.fruits.map((fruit: { id: any; name: any }) => (
-    <FruitListItem key={fruit.id} fruit={fruit} />
-  ));
-  return <ul>{fruitListItems}</ul>;
-}
-
-const data = [
-  { id: 1, name: 'apple' },
-  { id: 2, name: 'orange' },
-  { id: 3, name: 'blueberry' },
-  { id: 4, name: 'banana' },
-  { id: 5, name: 'kiwi' },
-];
-
-root.render(
-  <FruitList fruits={data} />
-);
+root.render(<Button />);
